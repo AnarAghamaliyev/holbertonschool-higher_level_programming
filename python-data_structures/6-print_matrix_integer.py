db7722c097df9f815c01cfd1d8f5eea1faf3d97d
AnarAghamaliyev <anaraghamaliyev7@gmail.com>
2026-08-18 22:37:43 +0000

    anar

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-# Brennan D Baraban <375@holbertonschool.com>
+"""Print a matrix of integers."""
 
 
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i != len(row) - 1:
+                print(" ", end="")
+        print()
