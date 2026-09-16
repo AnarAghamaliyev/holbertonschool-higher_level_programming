7fc20461012ffb896f094c0caadd3a4e2c33b31b
AnarAghamaliyev <anaraghamaliyev7@gmail.com>
2026-09-17 00:29:01 +0400

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Python web server"""
+
 import http.server
 import socketserver
 import json
 
 
 PORT = 8000
+
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
@@ -18,30 +21,32 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
+
             response_data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
             }
-            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+
+            self.wfile.write(
+                json.dumps(response_data).encode("utf-8")
+            )
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "status": "OK"
-            }"""
             self.wfile.write(b"OK")
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "error": "Endpoint not found"
-            }"""
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
+
 
 Handler = MyHandler
+
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
